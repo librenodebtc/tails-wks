22195adecf92f938ba6a11bfc7faecdab7405922
librenodebtc <66690645+librenodebtc@users.noreply.github.com>
2020-06-09 23:53:29 +0200

    download , verify and install latest version

--- wasabi/wasabi_tails.sh
+++ wasabi/wasabi_tails.sh
@@ -6,8 +6,28 @@
 ### Restore previos wallet folder
 ###
 
+# Import Public Key 
+curl https://raw.githubusercontent.com/zkSNACKs/WalletWasabi/master/PGP.txt | gpg --import
 
-sudo dpkg -i Wasabi-1.1.11.1.deb
+# Download latest release
+curl -s https://api.github.com/repos/zkSNACKs/WalletWasabi/releases/latest \
+| grep "browser_download_url.*deb" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+# Verify download
+deb_package_asc="$(find . -name "Wasabi*.deb.asc")"
+gpg --verify $deb_package_asc
+
+
+# Install
+deb_package="$(find . -name "Wasabi*.deb")"
+sudo dpkg -i $deb_package
+    
+    
+
+
 
 wassabee </dev/null &>/dev/null &
 
